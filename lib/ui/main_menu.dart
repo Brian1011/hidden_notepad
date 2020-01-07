4f19a3c88506ee985b783180ab10c3c451ff80bd
Brian Mutinda <mutindabrian26@gmail.com>
2020-01-07 14:31:52 +0300

    redesign card designs

--- lib/ui/main_menu.dart
+++ lib/ui/main_menu.dart
@@ -9,38 +9,36 @@ class _MainMenuState extends State<MainMenu> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Diary'),
+        title: const Text('My Diary',),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        // add gradient
       ),
       body: Container(
+            decoration: BoxDecoration(
+                color: Colors.grey[800]
+            ),
         child: ListView(
           children: <Widget>[
             Card(
-              elevation: 10,
+              color: Colors.black45,
+              elevation: 4,
               margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 5),
               child: ListTile(
-                leading: Icon(Icons.edit),
-                title: Text('Sweet Pie'),
-                subtitle: Text('7th Janauary 2020'),
+                leading: Icon(Icons.edit, color: Colors.grey[600],),
+                title: Text('Random Story...', style: TextStyle(color: Colors.white),),
+                subtitle: Text('7th Janauary 2020',style: TextStyle(color: Colors.white)),
                 trailing: Icon(Icons.delete, color: Colors.red,),
               ),
             ),
             Card(
-              elevation: 10,
+              color: Colors.black45,
+              elevation: 4,
               margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 5),
               child: ListTile(
-                leading: Icon(Icons.edit),
-                title: Text('Sweet Pie'),
-                subtitle: Text('7th Janauary 2020'),
-                trailing: Icon(Icons.delete, color: Colors.red,),
-              ),
-            ),
-            Card(
-              elevation: 10,
-              margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 5),
-              child: ListTile(
-                leading: Icon(Icons.edit),
-                title: Text('Sweet Pie...'),
-                subtitle: Text('7th Janauary 2020'),
+                leading: Icon(Icons.edit, color: Colors.grey[600],),
+                title: Text('Am grateful for', style: TextStyle(color: Colors.white),),
+                subtitle: Text('7th Janauary 2020',style: TextStyle(color: Colors.white)),
                 trailing: Icon(Icons.delete, color: Colors.red,),
               ),
             ),
@@ -49,8 +47,9 @@ class _MainMenuState extends State<MainMenu> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
-        child: Icon(Icons.add),
-        backgroundColor: Colors.green,
+        child: Icon(Icons.add, color: Colors.white,),
+        backgroundColor: Colors.grey,
+        elevation: 30,
       ),
     );
   }
