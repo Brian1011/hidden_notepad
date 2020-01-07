fa61bf37f679df7963489134fda06e96b373cfd1
Brian Mutinda <mutindabrian26@gmail.com>
2020-01-07 12:44:42 +0300

    create cards for diary

--- lib/ui/main_menu.dart
+++ lib/ui/main_menu.dart
@@ -9,7 +9,48 @@ class _MainMenuState extends State<MainMenu> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Note Taking'),
+        title: const Text('My Diary'),
+      ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 5),
+              child: ListTile(
+                leading: Icon(Icons.edit),
+                title: Text('Sweet Pie'),
+                subtitle: Text('7th Janauary 2020'),
+                trailing: Icon(Icons.delete, color: Colors.red,),
+              ),
+            ),
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 5),
+              child: ListTile(
+                leading: Icon(Icons.edit),
+                title: Text('Sweet Pie'),
+                subtitle: Text('7th Janauary 2020'),
+                trailing: Icon(Icons.delete, color: Colors.red,),
+              ),
+            ),
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 5),
+              child: ListTile(
+                leading: Icon(Icons.edit),
+                title: Text('Sweet Pie...'),
+                subtitle: Text('7th Janauary 2020'),
+                trailing: Icon(Icons.delete, color: Colors.red,),
+              ),
+            ),
+          ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
       ),
     );
   }
