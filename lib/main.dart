e55b916f4924ab56884e4abe81353b95fd7f8d59
Brian Mutinda <mutindabrian26@gmail.com>
2020-01-07 09:12:58 +0300

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:diary/screens/mainMenu.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: SplashScreen(),
+  initialRoute: '/main',
+  routes: {
+    '/main': (context)=>MainMenu(),
+  },
+));
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    await Future.delayed(Duration(seconds: 3));
+    navigationPage();
+  }
+
+  void navigationPage(){
+    Navigator.of(context).pushReplacementNamed('/main');
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}
---
+++ lib/screens/mainMenu.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class MainMenu extends StatefulWidget {
+  @override
+  _MainMenuState createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Note Taking'),
+      ),
+    );
+  }
+}
