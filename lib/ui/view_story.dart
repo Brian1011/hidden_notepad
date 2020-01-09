88584f5c177873bcc3e75feaa3229e042d9be494
Brian Mutinda <mutindabrian26@gmail.com>
2020-01-09 15:00:41 +0300

    create a view note page

---
+++ lib/ui/view_story.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ViewStory extends StatefulWidget {
+  @override
+  _ViewStoryState createState() => _ViewStoryState();
+}
+
+class _ViewStoryState extends State<ViewStory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
